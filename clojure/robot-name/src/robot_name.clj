007e11f8314c29624ee4e8f6be211d633b5b2663
Marc <marc@Macintosh.local>
2018-10-14 21:01:49 -0700

    robot-name almost done, reset-name still not working

--- clojure/robot-name/src/robot_name.clj
+++ clojure/robot-name/src/robot_name.clj
@@ -1,18 +1,32 @@
 (ns robot-name)
 
+(def names (atom #{}))
+
+(defn save-name [name]
+  (swap! names conj name))
+
+(defn name-exists? [name]
+  (contains? @names name))
+
 (defn gen-robot-name []
   (str
     (apply str (take 2 (repeatedly #(char (+ 65 (rand-int 26))))))
     (apply str (take 3 (repeatedly #(rand-int 10))))))
 
-(defn robot [] ;; <- arglist goes here
-  ;; your code goes here
-  )
+(defn gen-unique-robot-name []
+  (loop [name (gen-robot-name)]
+    (if (name-exists? name) (recur gen-robot-name)
+      name)))
 
-(defn robot-name [robot] ;; <- arglist goes here
-      ;; your code goes here
-      )
+(defn robot []
+  (let [name (gen-unique-robot-name)]
+    (save-name name)
+    (hash-map "name" name)))
 
-(defn reset-name [robot] ;; <- arglist goes here
-  ;; your code goes here
-  )
+(defn robot-name [robot]
+  (robot "name"))
+
+(defn reset-name [robot]
+  (let [name (gen-unique-robot-name)]
+    (save-name name)
+    (assoc robot "name" name)))
